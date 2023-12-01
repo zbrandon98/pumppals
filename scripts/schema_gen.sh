9d76e558121343aaecd0274cd9d95655b6287214
Austin Shih <austinhshih@gmail.com>
2023-11-30 19:32:27 -0800

    script working for linux

---
+++ scripts/schema_gen.sh
@@ -0,0 +1,6 @@
+cd ./prisma/models
+rm -rf ../schema.prisma
+(cat .config.prisma; echo; echo) >> ../schema.prisma
+for schema in *.prisma; do (cat "${schema}"; echo; echo) >> ../schema.prisma; done
+cd ..
+prisma format
