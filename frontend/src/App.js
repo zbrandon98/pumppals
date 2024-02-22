7a25f4a1b324c1be156a3f406d5560d9722c4056
zbrandon98 <bdzarate@ucdavis.edu>
2024-02-21 23:24:23 -0800

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.css';
+import Song from './components/song/Song';
+function App() {
+  return (
+    <div className="App">
+      <Song />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './App';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
+
